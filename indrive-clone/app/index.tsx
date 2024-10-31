74ac286c3fdc2d117598eb08007a36378d1c52b0
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-01 01:40:21 +0500

    Working..

--- indrive-clone/app/index.tsx
+++ indrive-clone/app/index.tsx
@@ -1,12 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text  } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+import MapView from 'react-native-maps';
+
+import * as Location from 'expo-location';
 
 const Home = () => {
+    const [location , setLocation] = useState<null | any>(null)
+    const [errorMsg , setErrorMsg] = useState<null | string>(null)
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestBackgroundPermissionsAsync();
+            if(status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            console.log(location);
+        });
+    },[]);
+
+    let text = 'Waiting..'
+    if(errorMsg) {
+        text = errorMsg;
+    }
+    else if (location) {
+        text = JSON.stringify(location);
+    }
   return (
-    <View>
-      <Text>Home</Text>
+    <View className='flex-1 items-center justify-center p-2'>
+      <Text className='text-xl text-center'>{text}</Text>
+      {location && <MapView className='w-full h-full' initialRegion={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0001,
+        longitudeDelta: 0.0001,
+      }}>
+    
+     </MapView>}
     </View>
   )
 }
 
 export default Home
+
+
+
+
+
