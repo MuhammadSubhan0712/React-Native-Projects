107f6e6aabefd531fd2ddfb91f21b99b048d3d1d
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-03 01:44:44 +0500

    Working..

--- indrive-clone/app/Login/index.tsx
+++ indrive-clone/app/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextInput, Text, TouchableOpacity, Image } from 'react-native';
+import { TextInput, Text, TouchableOpacity, Image, View } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Link } from 'expo-router';
@@ -12,14 +12,14 @@ const Login = () => {
     <SafeAreaProvider>
       <LinearGradient colors={['#6af913', '#0ebb00']} className="flex-1 items-center justify-center">
         <SafeAreaView className="w-11/12 bg-white p-5 rounded-2xl shadow-lg shadow-black border border-gray-300">
-        <Image className='h-1 w-1 rounded-lg'
-        source={require('')}
-        />
         
-    
+        <Image className='h-10 w-10 border rounded-lg'
+        source={{ uri:'https://play-lh.googleusercontent.com/CeO35pm-07SaLuthR7VVXwAl4mBA5thYagq2eB-VRflSEuW4eIArPvoM6PVw-nmnzw=w240-h480-rw' }}
+        />
           <Text className="text-center text-2xl font-bold text-green-700 mb-6 shadow-md shadow-green-400">
            Login
-          </Text>
+           </Text>
+           
           
           <TextInput
             className="h-12 px-4 rounded-lg bg-gray-200 mb-4 shadow-md shadow-gray-500 text-base text-gray-800"
