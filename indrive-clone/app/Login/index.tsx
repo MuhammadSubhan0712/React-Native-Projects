49052f550aa04728eae714ab975faf529be16a51
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-03 00:45:10 +0500

    Working..

--- indrive-clone/app/Login/index.tsx
+++ indrive-clone/app/Login/index.tsx
@@ -3,8 +3,8 @@ import {StyleSheet, TextInput} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Login = () => {
-  const [text, onChangeText] = React.useState('Useless Text');
-  const [number, onChangeNumber] = React.useState('');
+  
+
   const [email , setEmail] = useState('');
   const [password , setPassword] = useState('');
 
@@ -12,21 +12,17 @@ const Login = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView className='flex justify-center items-center'>
+     
         <TextInput
-          className='h-40 m-2 p-5'
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <TextInput
-          className='h-40 m-2 p-5'
-          onChangeText={onChangeNumber}
+          className=''
+        //   onChangeText={onChangeNumber}
           value={email}
           placeholder="Enter your Email"
           keyboardType="email-address"
         />
         <TextInput 
-          className='h-40 m-2 p-5'
-          onChangeText={onChangeNumber}
+          className=''
+        //   onChangeText={onChangeNumber}
           value={password}
           placeholder="****************"
           secureTextEntry={true}
