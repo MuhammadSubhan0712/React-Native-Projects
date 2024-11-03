79d9ed751f093d0fade93e05735da11699bf9236
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-04 02:02:31 +0500

    Working..

--- indrive-clone/app/Login/index.tsx
+++ indrive-clone/app/Login/index.tsx
@@ -1,68 +1,68 @@
-// import React, { useState } from 'react';
-// import { TextInput, Text, TouchableOpacity, Image, View } from 'react-native';
-// import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-// import { LinearGradient } from 'expo-linear-gradient';
-// import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import { TextInput, Text, TouchableOpacity, Image, View } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Link } from 'expo-router';
 
-// const Login = () => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-//   return (
-//     <SafeAreaProvider>
-//     <LinearGradient colors={['#abfe05', '#00db24']} className="flex-1 items-center justify-center">
-//       <SafeAreaView className="w-11/12 bg-white p-5 rounded-2xl shadow-lg shadow-black border border-gray-300">
+  return (
+    <SafeAreaProvider>
+    <LinearGradient colors={['#abfe05', '#00db24']} className="flex-1 items-center justify-center">
+      <SafeAreaView className="w-11/12 bg-white p-5 rounded-2xl shadow-lg shadow-black border border-gray-300">
         
-//         {/* Logo for the Login Screen */}
-//         <View className="flex items-center mt-2 mb-5">
-//           <Image
-//             className="h-24 w-24 border-4 border-gray-300 rounded-full shadow-md shadow-gray-500 mb-4"
-//             source={{
-//               uri: 'https://play-lh.googleusercontent.com/CeO35pm-07SaLuthR7VVXwAl4mBA5thYagq2eB-VRflSEuW4eIArPvoM6PVw-nmnzw=w240-h480-rw',
-//             }}
-//           />
-//         </View>
+        {/* Logo for the Login Screen */}
+        <View className="flex items-center mt-2 mb-5">
+          <Image
+            className="h-24 w-24 border-4 border-gray-300 rounded-full shadow-md shadow-gray-500 mb-4"
+            source={{
+              uri: 'https://play-lh.googleusercontent.com/CeO35pm-07SaLuthR7VVXwAl4mBA5thYagq2eB-VRflSEuW4eIArPvoM6PVw-nmnzw=w240-h480-rw',
+            }}
+          />
+        </View>
 
-//         <Text className="text-center py-3 text-2xl font-bold text-green-700 mb-6 shadow-md shadow-green-400">
-//           Login
-//         </Text>
+        <Text className="text-center py-3 text-2xl font-bold text-green-700 mb-6 shadow-md shadow-green-400">
+          Login
+        </Text>
            
-//                 {/* Email Input */}
-//           <TextInput
-//             className="h-12 px-4 rounded-lg bg-gray-200 mb-4 shadow-md shadow-gray-500 text-base text-gray-800"
-//             onChangeText={setEmail}
-//             value={email}
-//             placeholder="Enter your Email"
-//             placeholderTextColor="#aaa"
-//             keyboardType="email-address"
-//           />
+                {/* Email Input */}
+          <TextInput
+            className="h-12 px-4 rounded-lg bg-gray-200 mb-4 shadow-md shadow-gray-500 text-base text-gray-800"
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Enter your Email"
+            placeholderTextColor="#aaa"
+            keyboardType="email-address"
+          />
           
-//                 {/* Password Input */}
+                {/* Password Input */}
 
-//           <TextInput
-//             className="h-12 px-4 rounded-lg bg-gray-200 mb-4 shadow-md shadow-gray-500 text-base text-gray-800"
-//             onChangeText={setPassword}
-//             value={password}
-//             placeholder="************************"
-//             placeholderTextColor="#aaa"
-//             secureTextEntry
-//           />
+          <TextInput
+            className="h-12 px-4 rounded-lg bg-gray-200 mb-4 shadow-md shadow-gray-500 text-base text-gray-800"
+            onChangeText={setPassword}
+            value={password}
+            placeholder="************************"
+            placeholderTextColor="#aaa"
+            secureTextEntry
+          />
 
-//                     {/* Login Button */}
+                    {/* Login Button */}
 
-//           <TouchableOpacity className="bg-green-600 mt-4 py-2 rounded-lg shadow-lg shadow-green-900 active:bg-green-700">
-//             <Text className="text-lg font-semibold text-white text-center">Log In</Text>
-//           </TouchableOpacity>
+          <TouchableOpacity className="bg-green-600 mt-4 py-2 rounded-lg shadow-lg shadow-green-900 active:bg-green-700">
+            <Text className="text-lg font-semibold text-white text-center">Log In</Text>
+          </TouchableOpacity>
 
-//           <Link href={'/Register'} className='mt-4  py-3  hover:text-cyan-300'>
-//           <Text className='text-lg font-bold text-cyan-700 text-center'>
-//           Don't have an account
-//           </Text>
-//           </Link>
-//         </SafeAreaView>
-//       </LinearGradient>
-//     </SafeAreaProvider>
-//   );
-// };
+          <Link href={'/Register'} className='mt-4  py-3  hover:text-cyan-300'>
+          <Text className='text-lg font-bold text-cyan-700 text-center'>
+          Don't have an account
+          </Text>
+          </Link>
+        </SafeAreaView>
+      </LinearGradient>
+    </SafeAreaProvider>
+  );
+};
 
-// export default Login;
+export default Login;
--- indrive-clone/app/index.tsx
+++ indrive-clone/app/index.tsx
@@ -1,45 +1,63 @@
-import { View, Text  } from 'react-native'
-import React, { useEffect, useState } from 'react'
+// import { View, Text  } from 'react-native'
+// import React, { useEffect, useState } from 'react'
 
-import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
+// import MapView from 'react-native-maps';
+// import * as Location from 'expo-location';
+
+// const Home = () => {
+//     const [location , setLocation] = useState<null | any>(null)
+//     const [errorMsg , setErrorMsg] = useState<null | string>(null)
+
+//     useEffect(() => {
+//         (async () => {
+//             let {status} = await Location.requestForegroundPermissionsAsync();
+//             if(status !== 'granted') {
+//                 setErrorMsg('Permission to access location was denied');
+//                 return;
+//             }
+//             let location = await Location.getCurrentPositionAsync({});
+//             setLocation(location);
+//             console.log(location);
+//         });
+//     },[]);
+
+//     let text = 'Waiting......'
+//     if(errorMsg) {
+//         text = errorMsg;
+//     }
+//     else if (location) {
+//         text = JSON.stringify(location);
+//     }
+//   return (
+//     <View className='flex-1 items-center justify-center p-2'>
+//       <Text className='text-xl text-center'>{text}</Text>
+//       {location && <MapView className='w-full h-full' initialRegion={{
+//         latitude: location.coords.latitude,
+//         longitude: location.coords.longitude,
+//         latitudeDelta: 0.0001,
+//         longitudeDelta: 0.0001,
+//       }}>
+//      </MapView>}
+//     </View>
+//   )
+// }
+
+// export default Home
+
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import Login from './Login'
 
 const Home = () => {
-    const [location , setLocation] = useState<null | any>(null)
-    const [errorMsg , setErrorMsg] = useState<null | string>(null)
-
-    useEffect(() => {
-        (async () => {
-            let {status} = await Location.requestForegroundPermissionsAsync();
-            if(status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-            console.log(location);
-        });
-    },[]);
-
-    let text = 'Waiting......'
-    if(errorMsg) {
-        text = errorMsg;
-    }
-    else if (location) {
-        text = JSON.stringify(location);
-    }
   return (
-    <View className='flex-1 items-center justify-center p-2'>
-      <Text className='text-xl text-center'>{text}</Text>
-      {location && <MapView className='w-full h-full' initialRegion={{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.0001,
-        longitudeDelta: 0.0001,
-      }}>
     
-     </MapView>}
+    <View>
+    <Login/>
     </View>
+
+
   )
 }
 
@@ -47,19 +65,80 @@ export default Home
 
 
 
-// import { View, Text } from 'react-native'
-// import React from 'react'
-// import Login from './Login'
 
-// const Home = () => {
+
+
+
+
+// import { useState, useEffect } from 'react';
+// import { Platform, Text, View, StyleSheet } from 'react-native';
+// import MapView, { Marker } from 'react-native-maps';
+// import * as Location from 'expo-location';//Is line mein hum expo-location se location-related functions ko import kar rahe hain, jo location permissions aur current location lene mein madad karte hain.
+
+// export default function App() {
+//   const [location, setLocation] = useState<null | any>(null);
+//   const [errorMsg, setErrorMsg] = useState<null | string>(null);
+
+//   useEffect(() => {
+//     (async () => {
+
+//       let { status } = await Location.requestForegroundPermissionsAsync();//Yahan hum location permission ke liye request kar rahe hain aur response mein status le rahe hain.
+//       if (status !== 'granted') {
+//         setErrorMsg('Permission to access location was denied');
+//         return;
+//       }
+
+//       let location = await Location.getCurrentPositionAsync({}); //Yeh line current location ko retrieve kar rahi hai.
+//       setLocation(location);
+//       console.log(location);
+
+//     })();
+//   }, []);
+
+
+//   let text = 'Waiting..';
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     text = JSON.stringify(location);//Agar location mili, to text ko location data ko string mein convert karke assign kiya ja raha hai.
+//   }
+
 //   return (
-    
-//     <View>
-//     <Login/>
+//     <View style={styles.container}>
+//       <Text style={styles.paragraph}>{text}</Text>
+//       {location && <MapView style={styles.map} initialRegion={{
+//         latitude: location.coords.latitude,
+//         // Yahan hum initialRegion define kar rahe hain jo map ke initial latitude aur longitude set karta hai.
+//         longitude: location.coords.longitude,
+//         latitudeDelta: 0.0001,
+//         // Yeh map zoom level ko set kar raha hai.
+//         longitudeDelta: 0.0001,
+//       }}>
+//         <Marker coordinate={{
+//           latitude: location.coords.latitude,
+//           // Yahan hum Marker component ko render kar rahe hain, jo current location par point karega.
+//           longitude: location.coords.longitude,
+//         }} />
+
+//       </MapView>}
 //     </View>
-
-
-//   )
+//   );
 // }
 
-// export default Home
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 20,
+//     backgroundColor: 'lightblue'
+//   },
+//   paragraph: {
+//     fontSize: 18,
+//     textAlign: 'center',
+//   },
+//   map: {
+//     width: '100%',
+//     height: '80%'
+//   }
+// });
