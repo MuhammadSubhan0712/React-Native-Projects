6c924473232948affc5c721cbd5fa6a29be52787
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-04 23:28:09 +0500

    Working..

--- indrive-clone/app/Admin/index.tsx
+++ indrive-clone/app/Admin/index.tsx
@@ -1,7 +1,8 @@
-import { View, Text, ScrollView , FlatList } from 'react-native'
+import { View, Text, ScrollView , FlatList} from 'react-native'
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 import React from 'react'
+import { Link } from 'expo-router';
 
 
 const AdminPanel = () => {
@@ -9,23 +10,32 @@ const AdminPanel = () => {
     const DATA = [
         {
           id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-          Driver: 'First Item',
+          title: 'Driver',
         },
         {
           id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-          Customer: 'Second Item',
+          title: 'Driver',
         },
       ];
 
+      type ItemProps = {title: string};
+
+      const Item = ({title}: ItemProps) => (
+        <View className='bg-slate-600 my-3 mx-6 p-2'>
+          <Text className='text-3xl'>{title}</Text>
+        </View>
+      );
   return (
     <ScrollView>
     <SafeAreaProvider>
     <SafeAreaView className='flex mt-2'>
+    <Link href={''}>
       <FlatList
         data={DATA}
         renderItem={({item}) => <Item title={item.title} />}
         keyExtractor={item => item.id}
       />
+    </Link>
     </SafeAreaView>
   </SafeAreaProvider>
   </ScrollView>
@@ -33,16 +43,7 @@ const AdminPanel = () => {
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
+  container: 
   title: {
     fontSize: 32,
   },
