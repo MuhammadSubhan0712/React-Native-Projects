0e3005b5a0a64cf9a556db76818745e8828c8262
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-08 02:41:51 +0500

    Working...

--- indrive-clone/app/Booking/index.tsx
+++ indrive-clone/app/Booking/index.tsx
@@ -6,7 +6,11 @@ import * as Location from 'expo-location';
 export default function RideBooking() {
   const [location, setLocation] = useState<null | any>(null);
   const [errorMsg, setErrorMsg] = useState<null | string>(null);
-
+  const [region, setRegion] = useState<any>(null);
+  const [search , setSearch] = useState('');
+  const [places , setPlaces]  = useState<null | []>(null);
+  const [singlePlace , setSinglePlace] = useState<any>(null);
+  const [direction , setDirection] = useState<boolean>(false)
   useEffect(() => {
     (async () => {
 
@@ -15,11 +19,16 @@ export default function RideBooking() {
         setErrorMsg('Permission to access location was denied');
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({}); 
       setLocation(location);
-      console.log(location);
+      setRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta : 0.001,
+      longitudeDelta: 0.001,
+      })
 
+      console.log(location);
     })();
   }, []);
 
@@ -30,6 +39,42 @@ export default function RideBooking() {
   } else if (location) {
     text = JSON.stringify(location);
   }
+
+
+//    To Search specific place :
+const serachPlace = () => {
+    const option = {
+        method : 'GET',
+        headers:{
+            accept: 'application/json',
+            Authorization: 'fsq3qbL9ORBTq2ZaS6TUHxpAQZNDJjTlkT2lBeAynwmhZ8I='
+        }
+    };
+
+    fetch(`https://api.foursquare.com/v3/places/search?query=${search}&ll=${location.coords.latitude}%2C${location.coords.longitude}&radius=100000`,)
+    .then(res => res.json())
+    .then(res => {
+        setPlaces(res.result)
+    })
+     .catch(err => console.error(err));
+     console.log(search);
+    }
+
+    // For Single Place 
+    const singleplace = (item:any) =>{
+    setPlaces(null);
+    setSinglePlace({
+        latitude: item.geocodes.main.latitude,
+        longitude: item.geocodes.main.longitude
+    })
+    setRegion({
+        latitude:item.geocodes.main.latitude,
+        longitude: item.geocodes.main.longitude,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+    })
+    }
+
 
   return (
     <View className='flex justify-center items-center p-5 bg-black'>
